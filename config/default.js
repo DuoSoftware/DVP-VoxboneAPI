7b3fe0a230207ecb94e20665fe8cd25be6863137
waruna <waruna@duosoftware.com>
2017-10-26 15:51:55 +0530

    update dvp-common

--- config/default.js
+++ config/default.js
@@ -9,27 +9,43 @@ module.exports = {
     },
     "Mongo":
     {
-        "ip":"45.55.142.207",
+        "ip":"104.236.231.11",
         "port":"27017",
         "dbname":"dvpdb",
         "password":"DuoS123",
         "user":"duo"
     },
     "Redis":
-    {
-        "ip": "45.55.142.207",
-        "port": 6389,
-        "password":"DuoS123",
-        "redisdb":8,
-        "ardsData":6
-    },
+        {
+            "redisdb":8,
+            "ardsData":6,
+            "mode":"sentinel",//instance, cluster, sentinel
+            "ip": "45.55.142.207",
+            "port": 6389,
+            "user": "duo",
+            "db": 2,
+            "password": "DuoS123",
+            "sentinels":{
+                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+                "port":16389,
+                "name":"redis-cluster"
+            }
+
+        },
     "Security":
-    {
-        "ip": "45.55.142.207",
-        "port": 6389,
-        "user": "DuoS123",
-        "password": "DuoS123"
-    },
+        {
+
+            "ip" : "45.55.142.207",
+            "port": 6389,
+            "user": "duo",
+            "password": "DuoS123",
+            "mode":"sentinel",//instance, cluster, sentinel
+            "sentinels":{
+                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+                "port":16389,
+                "name":"redis-cluster"
+            }
+        },
     "Host": {
         "domain": "0.0.0.0",
         "port": 8832,
