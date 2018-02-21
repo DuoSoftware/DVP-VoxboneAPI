b81f2c5a4ab3a6591ac7c9692b027c0d09b9ef85
Heshan Indika <k.heshan.indika@gmail.com>
2018-02-21 14:19:53 +0530

    add missing custom-environment-variables.js fields

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -1,20 +1,21 @@
 module.exports = {
- "DB": {
-    "Type":"SYS_DATABASE_TYPE",
-    "User":"SYS_DATABASE_POSTGRES_USER",
-    "Password":"SYS_DATABASE_POSTGRES_PASSWORD",
-    "Port":"SYS_SQL_PORT",
-    "Host":"SYS_DATABASE_HOST",
-    "Database":"SYS_DATABASE_POSTGRES_USER"
-  },
-    "Mongo":
-    {
-        "ip":"SYS_MONGO_HOST",
-        "port":"SYS_MONGO_PORT",
-        "dbname":"SYS_MONGO_DB",
-        "password":"SYS_MONGO_PASSWORD",
-        "user":"SYS_MONGO_USER"
+    "DB": {
+        "Type":"SYS_DATABASE_TYPE",
+        "User":"SYS_DATABASE_POSTGRES_USER",
+        "Password":"SYS_DATABASE_POSTGRES_PASSWORD",
+        "Port":"SYS_SQL_PORT",
+        "Host":"SYS_DATABASE_HOST",
+        "Database":"SYS_DATABASE_POSTGRES_USER"
     },
+    "Mongo":
+        {
+            "ip":"SYS_MONGO_HOST",
+            "port":"SYS_MONGO_PORT",
+            "dbname":"SYS_MONGO_DB",
+            "password":"SYS_MONGO_PASSWORD",
+            "user":"SYS_MONGO_USER",
+            "replicaset" :"SYS_MONGO_REPLICASETNAME"
+        },
     "Host":
     {
         "domain": "HOST_NAME",
@@ -42,14 +43,37 @@ module.exports = {
         "walletservicePort": "SYS_WALLETSERVICE_PORT",
         "walletserviceVersion": "SYS_WALLETSERVICE_VERSION",
         "voxboneUrl":"VOXBONE_URL"
-    },"Security":
-    {
-        "ip": "SYS_REDIS_HOST",
-        "port": "SYS_REDIS_PORT",
-        "user": "SYS_REDIS_USER",
-        "password": "SYS_REDIS_PASSWORD"
+    },
+    "Redis":
+        {
+            "mode":"SYS_REDIS_MODE",
+            "ip": "SYS_REDIS_HOST",
+            "port": "SYS_REDIS_PORT",
+            "user": "SYS_REDIS_USER",
+            "password": "SYS_REDIS_PASSWORD",
+            "sentinels":{
+                "hosts": "SYS_REDIS_SENTINEL_HOSTS",
+                "port":"SYS_REDIS_SENTINEL_PORT",
+                "name":"SYS_REDIS_SENTINEL_NAME"
+            }
 
-    }
+        },
+
+    "Security":
+        {
+
+            "ip": "SYS_REDIS_HOST",
+            "port": "SYS_REDIS_PORT",
+            "user": "SYS_REDIS_USER",
+            "password": "SYS_REDIS_PASSWORD",
+            "mode":"SYS_REDIS_MODE",
+            "sentinels":{
+                "hosts": "SYS_REDIS_SENTINEL_HOSTS",
+                "port":"SYS_REDIS_SENTINEL_PORT",
+                "name":"SYS_REDIS_SENTINEL_NAME"
+            }
+
+        }
 
 };
 
